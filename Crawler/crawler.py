3f688a084da3b92162e2db23bbcd657a8b74ab3a
Gokhan Irmak <gokhanirmak2@hotmail.com>
2021-04-03 21:00:46 +0300

    css_selectors

--- Crawler/crawler.py
+++ Crawler/crawler.py
@@ -1,15 +1,51 @@
 import requests
 from bs4 import BeautifulSoup
-r=requests.get("http://python.beispiel.programmierenlernen.io/index.php")
+from urllib.parse import urljoin
+
+class CrawledArticle():
+    def __init__(self, title, emoji, content, image):
+        self.title = title
+        self.emoji = emoji
+        self.content = content
+        self.image = image
+
+
+class ArticleFetcher():
+    def fetch(self):
+        url = "http://python.beispiel.programmierenlernen.io/index.php"
+        r = requests.get(url)
+        doc = BeautifulSoup(r.text, "html.parser")
+
+        articles = []
+        for card in doc.select(".card"):
+            emoji = card.select_one(".emoji").text
+            content = card.select_one(".card-text").text
+            title = card.select(".card-title span")[1].text
+            imageUrl = card.select_one("img").attrs["src"]
+            image = urljoin(url,imageUrl)
+            crawled = CrawledArticle(title, emoji, content, image)
+            articles.append(crawled)
+
+        return articles
 
 # print(r.status_code)
 # print(r.headers)
 # print(r.text)
 
-doc = BeautifulSoup(r.text,"html.parser")
 
-for p in doc.find_all("p"):
-    print(p.text)
-    print(p.attrs) #Print attributes of 'p'
+# for p in doc.find_all("p"):
+#     print(p.text)
+#     print(p.attrs) #Print attributes of 'p'
+
+# Selecting with CSS Selectors
+# cards = doc.select(".card")
+# print(cards)
+
+# emojis = doc.select(".emoji")
+# print(emojis)
 
 
+
+fetcher = ArticleFetcher()
+fetched = fetcher.fetch()[0]
+print(fetched.image)
